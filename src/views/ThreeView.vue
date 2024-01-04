446a837540d710b78645b557fe70b3a5c0339964
baumi951 <79602318+baumi951@users.noreply.github.com>
2024-01-04 12:36:34 +0000

    add video example

--- src/views/ThreeView.vue
+++ src/views/ThreeView.vue
@@ -1,22 +1,41 @@
 <script setup lang="ts">
 
 import { TresCanvas } from '@tresjs/core';
+import { Color } from 'three';
 
 </script>
 
 <template>
 
-        
-        <TresCanvas>
-            
-            
-            
-        </TresCanvas>
+
+        <div id="myCanvasWrapper">
+
+                <TresCanvas clear-color="#82DBC5">
+
+                        <TresPerspectiveCamera :position="[4, 4, 4]" :look-at="[0, 0, 0]" />
+
+                        
+
+                        <TresMesh :position="[0, 1, 0]" :scale="[1, 2, 0.5]" :rotation="[Math.PI /4, 0, 0]">
+                                <TresBoxGeometry />
+
+                                <TresMeshBasicMaterial  :color="new Color('red')"/>
+                        </TresMesh>
+                        
+                        <TresAxesHelper />
+                </TresCanvas>
+        </div>
         
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
+#myCanvasWrapper {
+        margin: 0;
+        padding: 0;
+        height: 600px;
+        width: 600px;
+}
 
 
 </style>
