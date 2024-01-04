9f9c8b1f5f54e1de6653c3b88715dcf49dd8199d
baumi951 <79602318+baumi951@users.noreply.github.com>
2024-01-04 10:10:38 +0000

    threeview

---
+++ src/views/ThreeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+//import { TresCanvas } from '@tresjs/core';
+
+</script>
+
+<template>
+
+
+        
+        <!-- <TresCanvas>
+            
+            
+            
+        </TresCanvas> -->
+        
+</template>
+
+<style scoped lang="scss">
+
+
+
+</style>
